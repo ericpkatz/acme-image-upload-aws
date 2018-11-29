abcd9a13a5d160ff23f3b4bd0876eade7b99862b
Eric P. Katz <ericpkatz@gmail.com>
2018-11-29 13:28:29 -0500

    testing of Image

--- server/db/Image.js
+++ server/db/Image.js
@@ -1,4 +1,5 @@
 const S3 = require('../S3');
+const gm = require('gm').subClass({imageMagick: true});
 
 const conn = require('./conn');
 const Image = conn.define('image', {
@@ -12,33 +13,62 @@ const Image = conn.define('image', {
   },
 });
 
-Image.upload = function(data, bucketName){
+Image.parse = function(data){
+  const regex = /data:image\/(\w+);base64,(.*)/;
+  const matches = regex.exec(data);
+  const extension = matches[1];
+  const buffer = new Buffer(matches[2], 'base64');
+  return {
+    buffer, extension
+  };
+}
+Image.resize = function(bufferIn, extension){
   return new Promise((resolve, reject)=> {
-    const regex = /data:image\/(\w+);base64,(.*)/;
-    const matches = regex.exec(data);
-    if(!matches){
-      return reject(new Error('BASE64 ERROR'));
-    }
-    const extension = matches[1];
-    const image = this.build();
-    const Key = `${image.id.toString()}.${extension}`;
-    const Body = new Buffer(matches[2], 'base64');
-    S3.createBucket({ Bucket: bucketName}).promise()
+    gm(bufferIn)
+      .resize(null, 50)
+      .toBuffer(extension, (err, bufferOut)=> {
+        if(err){
+          return reject(err);
+        }
+        resolve(bufferOut);
+      });
+  });
+}
+
+Image.toAWS = function(buffer, extension, key, bucketName){
+    return S3.createBucket({ Bucket: bucketName}).promise()
       .then(()=> {
         return S3.putObject({
           Bucket: bucketName,
           ACL: 'public-read',
-          Body,
+          Body: buffer,
           ContentType: `image/${extension}`,
-          Key
+          Key: key
         }).promise();
       })
-      .then( ()=> {
-        image.url = `https://s3.amazonaws.com/${bucketName}/${Key}`;
-        return image.save();
-      })
-      .then( image => resolve(image))
-      .catch( ex => reject(ex));
+}
+
+Image.upload = function(data, bucketName){
+  return new Promise((resolve, reject)=> {
+    let buffer, extension;
+    try{
+      const result = this.parse(data);
+      buffer = result.buffer;
+      extension = result.extension;
+    }
+    catch(ex){
+      return reject(new Error('BASE64 ERROR'));
+    }
+    const image = this.build();
+    const Key = `${image.id.toString()}.${extension}`;
+    this.resize(buffer, extension)
+    .then( buffer => this.toAWS(buffer, extension, Key, bucketName))
+    .then( ()=> {
+      image.url = `https://s3.amazonaws.com/${bucketName}/${Key}`;
+      return image.save();
+    })
+    .then( image => resolve(image))
+    .catch( ex => reject(ex));
   });
 };
 module.exports = Image;
--- tests/server/models.js
+++ tests/server/models.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const { syncAndSeed, models } = require('../../server/db');
 const { User, Image } = models; 
+const sinon = require('sinon');
 
 describe('models', ()=> {
   beforeEach(()=> syncAndSeed());
@@ -19,10 +20,25 @@ describe('models', ()=> {
     });
 
     describe('with valid image', ()=> {
+      let parse, toAWS, resize;
+      beforeEach(()=> {
+        parse = sinon.stub(Image, 'parse');
+        parse.withArgs('ABC').returns({ buffer: 'DEF', extension: 'png'});
+
+        resize = sinon.stub(Image, 'resize');
+        resize.withArgs('DEF', 'png').returns(Promise.resolve('RESIZED'));
+
+        toAWS = sinon.stub(Image, 'toAWS');
+        toAWS.withArgs('RESIZED', 'png').returns(Promise.resolve());
+      });
+      afterEach(()=> {
+        parse.restore();
+        resize.restore();
+        toAWS.restore();
+      });
       it('can be uploaded', ()=> {
-        const smiley = require('../../server/db/_smiley');
-        return Image.upload(smiley, 'com.erickatz.a.bucket')
-          .then( image => expect(image.url).to.equal(`https://s3.amazonaws.com/com.erickatz.a.bucket/${image.id}.jpeg`));
+        return Image.upload('ABC', 'bucket')
+          .then( image => expect(image.url).to.equal(`https://s3.amazonaws.com/bucket/${image.id}.png`));
       });
     });
   });
